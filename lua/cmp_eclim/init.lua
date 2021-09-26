f656f385fd113ad71d6df9141478ed785d857144
minhduc0711 <phamminhduc0711@gmail.com>
2021-09-26 23:29:17 +0700

    Get it partially working..

---
+++ lua/cmp_eclim/init.lua
@@ -0,0 +1,64 @@
+local cmp = require("cmp")
+local kinds_mapping = {
+  v = cmp.lsp.CompletionItemKind.Variable,
+  f = cmp.lsp.CompletionItemKind.Method,
+  t = cmp.lsp.CompletionItemKind.Class,
+  k = cmp.lsp.CompletionItemKind.Keyword
+}
+local source = {}
+
+source.new = function()
+  return setmetatable({}, { __index = source })
+end
+
+function source:is_available()
+  return vim.fn['eclim#PingEclim'](0) == 0 and false or true
+end
+
+function source:get_debug_name()
+  return 'cmp-eclim'
+end
+
+function source:get_trigger_characters(_)
+  -- TODO: no clue how this works atm..
+  -- I thought completion would only trigger after a dot if I do this?
+  return { '.' }
+end
+
+function source:complete(params, callback)
+  local offset = vim.fn['eclim#java#complete#CodeComplete'](1, '') + 1
+  local input = string.sub(params.context.cursor_before_line, offset)
+
+  -- TODO: ugly workaround to make eclim use the right column offset when retrieving completions
+  -- ( col('.') behaves differently between 2 CodeComplete calls for some reason when using <C-x><C-u>)
+  -- Set cursor to the column where completion starts
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.api.nvim_win_set_cursor(0, {row, col - string.len(input)})
+  local results = vim.fn['eclim#java#complete#CodeComplete'](0, input)
+  -- Reset original cursor location
+  vim.api.nvim_win_set_cursor(0, {row, col})
+
+  local items = {}
+  -- TODO: CodeComplete returns -1 when completing imports
+  if type(results) == "table" then
+    for _, item in ipairs(results) do
+      table.insert(items, {
+        label = item.word,
+        dup = item.dup,
+        insertText = item.word,
+        kind = kinds_mapping[item.kind],
+        labelDetails = {
+          description = item.menu,
+        },
+        documentation = item.info,
+      })
+    end
+  end
+
+  callback({
+    items = items,
+    isIncomplete = true
+  })
+end
+
+return source
